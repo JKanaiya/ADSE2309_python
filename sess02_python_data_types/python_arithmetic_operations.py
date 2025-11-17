dce8e542d9d53e53136952d87a615d8eec6cc2e7
Hasan <m.dossa@edulink.ac.ke>
2025-11-17 14:01:50 +0300

    Added file that shows arithmetic operations with python

---
+++ sess02_python_data_types/python_arithmetic_operations.py
@@ -0,0 +1,32 @@
+# Python script to demonstrate the various arithmetic operations on numeric values
+# Declare and get 2 values from the user
+num1 = int(input('Enter the first number to be used in the calculation: \n'))
+num2 = int(input('Enter the first number to be used in the calculation: \n'))
+
+# Addition
+sum = num1 + num2
+
+# Subtraction
+difference = num1 - num2
+
+# Multiplication
+product = num1 * num2
+
+# Division
+floating_division = num1 / num2
+integer_division = num1 // num2
+
+# Modulus
+modulus = num1 % num2
+
+# Exponentiation
+exponent = num1 ** num2
+
+# Display the results
+print(f"Addition: {num1} + {num2} = {sum}")
+print(f"Subtraction: {num1} - {num2} = {difference}")
+print(f"Multiplication: {num1} * {num2} = {product}")
+print(f"Floating Division: {num1} / {num2} = {floating_division}")
+print(f"Integer Division: {num1} /{num2} = {integer_division}")
+print(f"Modulus: {num1} % {num2} = {modulus}")
+print(f"Exponent: {num1} ** {num2} = {exponent}")
